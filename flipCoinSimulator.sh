677927dfd0a20908acc6f46eed509de0fadd3a1b
balaji38738 <bijjapwar123@gmail.com>
2020-03-21 22:50:19 +0530

    Found percentage of triplet combinations

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,7 +9,7 @@ echo "Flip Coin Simulation"
 HEAD=0
 TAIL=1
 
-declare -A singletCoinComb	doubletCoinComb
+declare -A singletCoinComb	doubletCoinComb tripletCoinComb
 
 #Function to generate singlet coin combination
 function generateSingletComb() {
@@ -22,7 +22,7 @@ function generateSingletComb() {
       	$HEAD) combination="H";;
       	$TAIL) combination="T";;
    	esac
-	singletCoinComb[$combination]=$(( ${singletCoinComb[$combination]} + 1 ))
+	((singletCoinComb[$combination]+=1))
 	done
 }
 
@@ -41,8 +41,27 @@ function generateDoubletComb() {
 				$TAIL) combination="T$combination";;
    		esac
 		done
-   	doubletCoinComb[$combination]=$(( ${doubletCoinComb[$combination]} + 1 ))
+   	((doubletCoinComb[$combination]+=1))
 	done
+}
+
+#Function to generate triplet coin combination
+function generateTripletComb() {
+   tripletCoinComb=( ["HHH"]=0 ["HHT"]=0 ["HTH"]=0 ["HTT"]=0 ["TTT"]=0 ["THT"]=0 ["TTH"]=0 ["THH"]=0  )
+   maxFlips=$1
+   for (( flip=0; flip<maxFlips; flip++ ))
+   do
+      combination=""
+      for (( coin=1; coin<=3; coin++ ))
+      do
+         face=$(( RANDOM % 2 ))
+         case $face in
+            $HEAD) combination="H$combination";;
+            $TAIL) combination="T$combination";;
+         esac
+      done
+      ((tripletCoinComb[$combination]+=1))
+   done
 }
 
 
@@ -62,16 +81,22 @@ function calcPercent() {
          	doubletCoinComb[$comb]=`echo "scale=3; ${doubletCoinComb[$comb]} * 100 / $maxFlips" | bc`
       	done
       	;;
+		3)
+			for comb in "${!tripletCoinComb[@]}"
+         do
+            tripletCoinComb[$comb]=`echo "scale=3; ${tripletCoinComb[$comb]} * 100 / $maxFlips" | bc`
+         done
+         ;;
 	esac
 }
 
 function getUserChoice() {
-	read -p "How many coins you want flip ? 1 or 2: " noOfCoins
-	if [[ $noOfCoins -ne 1 && $noOfCoins -ne 2 ]]
+	read -p "How many coins you want flip ? 1 to 3: " noOfCoins
+	if [[ $noOfCoins -ne 1 && $noOfCoins -ne 2 && $noOfCoins -ne 3 ]]
 	then
 		getUserChoice
 	else
-		read -p "Enter no of flips of each coin: " totalFlips
+		read -p "Enter number of flips of each coin: " totalFlips
 	fi
 }
 
@@ -92,7 +117,13 @@ do
 			echo "Combinations: ${!doubletCoinComb[@]}"
 			echo "Count: ${doubletCoinComb[@]}"
          calcPercent $noOfCoins $totalFlips
-         echo "Percentage: ${doubletCoinComb[@]}"
+         echo "Percentage: ${doubletCoinComb[@]}";;
+		3)
+			generateTripletComb $totalFlips
+         echo "Combinations: ${!tripletCoinComb[@]}"
+         echo "Count: ${tripletCoinComb[@]}"
+         calcPercent $noOfCoins $totalFlips
+         echo "Percentage: ${tripletCoinComb[@]}";;
 	esac
 	read -p "Do you want to continue ?: " play
 done
