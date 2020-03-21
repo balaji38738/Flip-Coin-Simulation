cd8ca91b0743d4f2422c586f9cf88a08e745a795
balaji38738 <bijjapwar123@gmail.com>
2020-03-21 09:49:36 +0530

    Master branch created

---
+++ flipCoinSimulator.sh
@@ -0,0 +1,7 @@
+#! /bin/bash -x
+
+#Problem Statement:- Display Winning percentage of Head or Tail Combination in a Singlet, Doublet and Triplet
+#Author:- Balaji Ijjapwar
+#Date:- 20 March 2020
+
+echo "Flip Coin Simulation"
