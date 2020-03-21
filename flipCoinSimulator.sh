708b58f7466504313cb637fa36a28733e371ed8e
balaji38738 <bijjapwar123@gmail.com>
2020-03-21 22:16:25 +0530

    Found percentage of doublet combinations

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -9,33 +9,90 @@ echo "Flip Coin Simulation"
 HEAD=0
 TAIL=1
 
-declare -A singletCoinComb	#Stores singlet coin combinations
-
-singletCoinComb=( ["H"]=0 ["T"]=0 )
+declare -A singletCoinComb	doubletCoinComb
 
 #Function to generate singlet coin combination
 function generateSingletComb() {
-   read -p "How many times you want to flip 1 coin: " maxFlips
+	singletCoinComb=( ["H"]=0 ["T"]=0 )
+   maxFlips=$1
    for (( flip=0; flip<maxFlips; flip++ ))
    do
       face=$(( RANDOM % 2 ))
       case $face in
-      $HEAD) combination="H";;
-      $TAIL) combination="T";;
-   esac
+      	$HEAD) combination="H";;
+      	$TAIL) combination="T";;
+   	esac
 	singletCoinComb[$combination]=$(( ${singletCoinComb[$combination]} + 1 ))
 	done
 }
 
-function calcPercent() {
-	for comb in "${!singletCoinComb[@]}"
+#Function to generate doublet coin combination
+function generateDoubletComb() {
+	doubletCoinComb=( ["HH"]=0 ["HT"]=0 ["TT"]=0 ["TH"]=0 )
+	maxFlips=$1
+	for (( flip=0; flip<maxFlips; flip++ ))
    do
-      singletCoinComb[$comb]=`echo "scale=3; ${singletCoinComb[$comb]} * 100 / $maxFlips" | bc`
-   done
+		combination=""
+		for (( coin=1; coin<=2; coin++ ))
+		do
+      	face=$(( RANDOM % 2 ))
+      	case $face in
+				$HEAD) combination="H$combination";;
+				$TAIL) combination="T$combination";;
+   		esac
+		done
+   	doubletCoinComb[$combination]=$(( ${doubletCoinComb[$combination]} + 1 ))
+	done
 }
 
-generateSingletComb
-calcPercent
 
-echo "Combinations: ${!singletCoinComb[@]}"
-echo "Percentages: ${singletCoinComb[@]}"
+function calcPercent() {
+	coins=$1
+	maxFlips=$2
+	case $coins in
+		1)
+			for comb in "${!singletCoinComb[@]}"
+   		do
+      		singletCoinComb[$comb]=`echo "scale=3; ${singletCoinComb[$comb]} * 100 / $maxFlips" | bc`
+   		done
+			;;
+		2)
+			for comb in "${!doubletCoinComb[@]}"
+      	do
+         	doubletCoinComb[$comb]=`echo "scale=3; ${doubletCoinComb[$comb]} * 100 / $maxFlips" | bc`
+      	done
+      	;;
+	esac
+}
+
+function getUserChoice() {
+	read -p "How many coins you want flip ? 1 or 2: " noOfCoins
+	if [[ $noOfCoins -ne 1 && $noOfCoins -ne 2 ]]
+	then
+		getUserChoice
+	else
+		read -p "Enter no of flips of each coin: " totalFlips
+	fi
+}
+
+play="y"	#If user wants to play
+
+while [ $play == "y" ]
+do
+	getUserChoice
+	case $noOfCoins in
+      1)
+			generateSingletComb $totalFlips
+			echo "Combinations: ${!singletCoinComb[@]}"
+			echo "Count: ${singletCoinComb[@]}"
+			calcPercent $noOfCoins $totalFlips
+			echo "Percentage: ${singletCoinComb[@]}";;
+      2)
+			generateDoubletComb $totalFlips
+			echo "Combinations: ${!doubletCoinComb[@]}"
+			echo "Count: ${doubletCoinComb[@]}"
+         calcPercent $noOfCoins $totalFlips
+         echo "Percentage: ${doubletCoinComb[@]}"
+	esac
+	read -p "Do you want to continue ?: " play
+done
